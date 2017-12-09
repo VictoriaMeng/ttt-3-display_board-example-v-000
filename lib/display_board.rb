cacbeaadf9f4bffb7800c701a1075ccd70d41d10
Victoria Meng <nights1ststar@protonmail.com>
2017-12-09 00:53:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  row = ""
-  cell = "   "
-  pipe = "|"
-  dash = "-----------"
-
-  row.push(cell).push(pipe).push(cell).push(pipe).push(cell)
-
-  puts row
+puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
 
 end
 
